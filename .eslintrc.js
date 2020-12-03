f18e1efa68a573c6a58b07265ce1d0d3abaf9f0c
Heikal Syah Shiddiq <slavyan@secret.fyi>
2020-12-04 01:48:24 +0700

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,7 @@ module.exports = {
         'linebreak-style': [
             'error',
             'windows'
-            ],
+        ],
         'quotes': [
             'error',
             'single'
